c4c310ec9cf8bacca8a44efeacae0ff5fbccc765
ranjith4442 <154911695+ranjith4442@users.noreply.github.com>
2023-12-29 07:05:22 +0530

    Add files via upload

---
+++ TASK6.java
@@ -0,0 +1,27 @@
+
+public class TASK6 {
+
+	public static void main(String[] args) {
+		       
+		        for(int a=5;a>=1;a--) 
+		        { 
+		            for(int b=5;b>=1;b--) 
+		            { 
+		                if(a>=b) 
+		                { 
+		                    System.out.print(a); 
+		                } 
+		                else 
+		                { 
+		                    System.out.print(b); 
+		                } 
+		            } 
+		            System.out.println(); 
+		        }
+		    }
+		}
+
+
+	
+
+
